736eb4af73bf21f6035e722fe43b486bd7ac5851
周洲游 <1445352275@qq.com>
2019-03-31 21:17:12 +0800

    Integer Reverse

--- src/LeetCode/ReverseInteger.java
+++ src/LeetCode/ReverseInteger.java
@@ -1,4 +1,19 @@
 package LeetCode;
 
 public class ReverseInteger {
+    public int reverse(int x) {
+        long out = 0;
+        boolean flag = x < 0;
+        if (flag) x = 0 - x;
+        while (x > 0) {
+            int mod = x % 10;
+            out *= 10;
+            out += mod;
+            if (out > Integer.MAX_VALUE) return 0;
+            if (out < Integer.MIN_VALUE) return 0;
+            x /= 10;
+        }
+        out = (flag) ? 0 - out : out;
+        return (int) out;
+    }
 }
