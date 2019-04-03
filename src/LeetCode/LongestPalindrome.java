203f71662ad9e1eaec7d31d311973e754644d7d3
周洲游 <1445352275@qq.com>
2019-04-03 16:19:46 +0800

    LongestPalindrome

--- src/LeetCode/LongestPalindrome.java
+++ src/LeetCode/LongestPalindrome.java
@@ -16,27 +16,46 @@ public class LongestPalindrome {
      */
 
     public String longestPalindrome(String s) {
-        for (int step = s.length(); step > 0 ; step--) {
-            int left = 0;
-            int right = left + step - 1;
-            while (right < s.length()) {
-                boolean flag = true;
-                for (int i = 0; i < step / 2; i++) {
-                    if (s.charAt(left + i) != s.charAt(right - i)) {
-                        flag = false;
-                        break;
-                    }
-                }
-                if (flag) return s.substring(left, right+1);
-                left++;
-                right++;
-            }
+        StringBuilder temp = new StringBuilder("~|");
+        for (int i = 0; i < s.length(); i++) {
+            temp.append(s.charAt(i));
+            temp.append("|");
         }
-        return s.substring(0, 0);
+        String str = temp.toString();
+        int[] array = new int[str.length()];
+        int max = 0;
+        for (int i = 0; i < str.length(); i++) {
+            array[i] = getLength(str, i);
+        }
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] > array[max]) max = i;
+        }
+        int left = (max-array[max]) / 2;
+        int right = left + array[max] -1 ;
+        return s.substring(left, right);
     }
 
+    int getLength(String s, int location) {
+        int n = s.length();
+        int ans = 1;
+        if (location == 0 || location == n-1) return 1;
+        int i = location-1, j = location+1;
+        while (i >= 0 && j < n) {
+            if (s.charAt(i) == s.charAt(j)) {
+                i--;
+                j++;
+                ans++;
+            } else {
+                return ans;
+            }
+        }
+        return ans;
+    }
+
+
+
     public static void main(String[] args) {
-        String a = "daaaaad";
+        String a = "12321";
         System.out.println(new LongestPalindrome().longestPalindrome(a));
     }
 }
