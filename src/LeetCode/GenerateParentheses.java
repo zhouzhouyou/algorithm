01ecd2c020997777af7f8eb847f807b8ec71768a
周洲游 <1445352275@qq.com>
2019-08-31 19:38:04 +0800

    简单的回溯~

---
+++ src/LeetCode/GenerateParentheses.java
@@ -0,0 +1,21 @@
+package LeetCode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GenerateParentheses {
+    public List<String> generateParenthesis(int n) {
+        List<String> list = new ArrayList<>();
+        backtrack(list, "", 0, 0, n);
+        return list;
+    }
+
+    public void backtrack(List<String> list, String str, int open, int close, int n) {
+        if (str.length() == 2 * n) {
+            list.add(str);
+            return;
+        }
+        if (open < n) backtrack(list, str+"(", open+1, close, n);
+        if (close < open) backtrack(list, str+")", open, close+1, n);
+    }
+}
