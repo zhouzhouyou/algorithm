6f1f2fac714e055e1620903a0e7ef6c65c211173
周洲游 <1445352275@qq.com>
2019-08-31 11:34:25 +0800

    虽然一次过了，时间less than 100%，但空间只less than 16%

---
+++ src/LeetCode/MergeTwoSortedList.java
@@ -0,0 +1,44 @@
+package LeetCode;
+
+public class MergeTwoSortedList {
+    public class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) {
+            val = x;
+        }
+    }
+
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode head = null, rear = null;
+        if (l1 == null) return l2;
+        else if (l2 == null) return l1;
+        while (l1 != null || l2 != null) {
+            if (l1 == null || l2 == null) {
+                if (l1 == null) rear.next = l2;
+                else rear.next = l1;
+                break;
+            }
+            if (l1.val < l2.val) {
+                if (head == null) {
+                    head = l1;
+                    rear = head;
+                } else {
+                    rear.next = l1;
+                    rear = rear.next;
+                }
+                l1 = l1.next;
+            } else {
+                if (head == null) {
+                    head = l2;
+                    rear = head;
+                } else {
+                    rear.next = l2;
+                    rear = rear.next;
+                }
+                l2 = l2.next;
+            }
+        }
+        return head;
+    }
+}
--- src/LeetCode/ValidParentheses.java
+++ src/LeetCode/ValidParentheses.java
@@ -7,11 +7,11 @@ import java.util.Stack;
 public class ValidParentheses {
     public boolean isValid(String s) {
         Map<Character, Character> map = new HashMap<>();
-        map.put('(',')');
+        //map.put('(',')');
         map.put(')','(');
-        map.put('[',']');
+       // map.put('[',']');
         map.put(']','[');
-        map.put('{','}');
+        //map.put('{','}');
         map.put('}','{');
         Stack<Character> stack = new Stack<>();
         for (char c : s.toCharArray()) {
